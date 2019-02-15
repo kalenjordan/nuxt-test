c8f55e92756c050a59ee35369a2cf9476db1e8ae
Kalen Jordan <kalen@magemail.co>
2019-02-15 15:33:53 -0600

    Auth

---
+++ pages/auth.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="container">
+        <div>
+            <h1 class="title">
+                auth, {{ user.name }}
+            </h1>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: { name: 'not logged in' }
+            }
+        },
+        async mounted() {
+            let auth = 'api_token=' + this.$route.query.api_token;
+            this.$axios.get('https://local.pros.global.test/api/v1/me?' + auth).then((response) => {
+                this.user = response.data;
+                this.$cookies.set('user', JSON.stringify(response.data));
+            });
+        }
+    }
+</script>
