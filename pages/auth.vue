2833e37a159a28e569c0cb3d67116cbb3ffac0a7
Kalen Jordan <kalen@magemail.co>
2019-02-17 19:28:22 -0600

    Log api url

--- pages/auth.vue
+++ pages/auth.vue
@@ -16,6 +16,7 @@
             }
         },
         async mounted() {
+            console.log('api url: ' + process.env.API_URL);
             let url = process.env.API_URL + 'me?api_token=' + this.$route.query.api_token;
             this.$axios.get(url).then((response) => {
                 this.user = response.data;
